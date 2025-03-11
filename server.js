ed0b658d1808ecfaae3435ef5c9b2731fe8b4289
dhanrajpimple <dhanrajpimple16012001@gmail.com>
2025-03-11 08:46:11 +0530

    for building comand

--- server.js
+++
@@ -1,7 +0,0 @@
-import { createRequestHandler } from "@remix-run/cloudflare";
-
-export default {
-  async fetch(request, env, ctx) {
-    return createRequestHandler({ build: await import("./build/server.js") })(request, env, ctx);
-  },
-};
